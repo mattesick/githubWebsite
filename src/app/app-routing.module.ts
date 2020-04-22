aeaad17ddd8ff820a15b7d2cd881acf27b8907d6
Heraghty <aheraghty@dxc.com>
2020-04-22 11:31:59 +0100

    added project skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TeamComponent } from './team/team.component';
-import { PersonComponent } from './person/person.component'
+import { PersonComponent } from './person/person.component';
+import { ProjectComponent } from './projects/project.component';
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
   { path: 'Teams', component: TeamComponent },
-  { path: 'Projects', component: HomeComponent },
+  { path: 'Projects', component: ProjectComponent },
   { path: 'MobilityApps', component: HomeComponent },
   { path: 'Person/:id', component: PersonComponent}
+
 ];
 
 @NgModule({
