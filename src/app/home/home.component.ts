2f314e3d3c20b54ee66eee0ff8d23a89fb7200e0
jEF420 <jEF420@users.noreply.github.com>
2020-04-28 19:15:43 +0200

    Upcoming

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,17 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  faChevronDown = faChevronDown;
   ngOnInit(): void {
+  }
+  dropDown() {
+    var x = document.getElementById("drop");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+
+    }
   }
 
 }
