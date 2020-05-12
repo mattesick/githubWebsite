15102e565580e9534602743357193258c62106d5
jEF420 <jEF420@users.noreply.github.com>
2020-05-12 12:57:04 +0200

    some stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,9 @@ const routes: Routes = [
   { path: 'MobilityApps', component: HomeComponent },
   { path: 'Person/:id', component: PersonComponent },
   { path: 'Search/:query', component: SearchComponent },
+  { path: 'Search/', component: SearchComponent },
   { path: '**', component: PageNotFoundComponent }
+  
 
 ];
 
