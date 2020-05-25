147f837167ec1798fc64b4bb36976c877061b207
mattesick <mattiassjonell@gmail.com>
2020-05-25 10:54:51 +0200

    ghpages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL:"https://55682283.ngrok.io/",
+  API_URL:"https://78bd2978.ngrok.io/",
   WEBSITE_URL:"https://mattesick.github.io/githubWebsite/"
 };
